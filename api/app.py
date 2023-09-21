d4229d85b96f5534b33b2ef5e7df83de5dc30fed
Austin Hendricks <ahendricks@bbw.com>
2023-09-21 15:00:31 -0400

    Changed line endings from CRLF to LF, added new route with sample page

--- api/app.py
+++ api/app.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+import random
+from flask import Flask, jsonify
 from elasticsearch import Elasticsearch
 import time
 
 # For testing purposes, we will want to change how this is stored in actual backend
-ELASTIC_PASSWORD = 'cse5914_capstone'
+ELASTIC_PASSWORD = "cse5914_capstone"
 
 elastic_client = Elasticsearch(
-    'https://es01:9200',
-    ca_certs='/app/certs/ca/ca.crt',
-    basic_auth=('elastic', ELASTIC_PASSWORD)
+    "https://es01:9200",
+    ca_certs="/app/certs/ca/ca.crt",
+    basic_auth=("elastic", ELASTIC_PASSWORD),
 )
 
 app = Flask(__name__)
 
-@app.route('/time')
+
+@app.route("/time")
 def get_current_time():
-    return {'time': time.time()}
+    return {"time": time.time()}
+
 
 # Demo on how to get info from the elasticsearch client
-@app.route('/info')
+@app.route("/info")
 def get_elastic_info():
     global elastic_client
     return elastic_client.info(pretty=True).body
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
-import { useState, useEffect } from 'react'
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import SamplePage from './pages/SamplePage'
 
-function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-  const [esResponse, setESResponse] = useState();
-
-  useEffect(() => {
-    // Get current time from backend
-    fetch('/time').then((response) => response.json())
-    .then((data) => {
-      setCurrentTime(data.time);
-    });
-
-    // Request info on the elasticsearch container
-    fetch('/info').then((response) => response.text())
-    .then((data) => {
-      setESResponse(data)
-    });
-  }, []);
-
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The time is { currentTime }.</p>
-        <p>Response from ElasticSearch { esResponse } </p>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div className='App'>
+        <Routes>
+          <Route exact path='/' element={<HomePage />} />
+          <Route exact path='/sample' element={<SamplePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
