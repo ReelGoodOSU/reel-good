66680658715cf5910b5ff92e2e39e78905c4121a
Joshua VonSeggern <joshvon44@gmail.com>
2023-09-25 13:02:35 -0400

    Create sample approach for API backend for searching

--- api/app.py
+++ api/app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, request
 from elasticsearch import Elasticsearch
 import time
 
 # For testing purposes, we will want to change how this is stored in actual backend
 ELASTIC_PASSWORD = "cse5914_capstone"
 
-elastic_client = Elasticsearch(
+ES = Elasticsearch(
     "https://es01:9200",
     ca_certs="/app/certs/ca/ca.crt",
     basic_auth=("elastic", ELASTIC_PASSWORD),
@@ -24,8 +24,29 @@ def get_current_time():
 # Demo on how to get info from the elasticsearch client
 @app.route("/info")
 def get_elastic_info():
-    global elastic_client
-    return elastic_client.info(pretty=True).body
+    global ES
+    return ES.info(pretty=True).body
+
+
+# TEMP NOTE: WE WILL BE USING GET REQUEST FOR SEARCHING BECAUSE WE ARE NOT MODIFY DATA, WE ARE SEARCHING AND GETTING DATA BACK
+@app.route("/search")
+def search_elastic():
+    global ES
+    # TODO HOW SHOULD I FORMAT AND RETURN THE DATA
+    request.args
+    query = request.args.get('query')
+    return ES.search(index="movies", query={"match": {"credits": {"query": query}}}).body
+
+
+"""
+For the backend this is what I need to know to handle a search query and returning the result
+
+1. How is the data from the search form returned to flask?
+2. How do I determine the field I perform the query on (actor, genre, title, etc)?
+3. How do I know the name of fields of data items?
+4. How should I format the data to be returned?
+5.
+"""
 
 
 if __name__ == "__main__":
