b804b229f901ebb4b3fe42b1aaf97742f4f628b3
Joshua VonSeggern <joshvon44@gmail.com>
2023-09-25 22:41:02 -0400

    Implement basic search frontend to show off how this might be done

--- src/pages/SamplePage.js
+++ src/pages/SamplePage.js
@@ -4,115 +4,75 @@ import React, { useReducer, useState } from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
 
-const formReducer = (state, event) => {
-  if (event.reset) {
-    return {
-      apple: "",
-      count: 0,
-      name: "",
-      "gift-wrap": false,
-    };
-  }
+// This function will update the JSON representation of the modifed value
+function formReducer(state, event) {
+  // event stores the name and value of the modified field, update it and leave
+  // everything else unmodified
   return {
     ...state,
     [event.name]: event.value,
   };
-};
+}
 
-function MyForm() {
-  const [formData, setFormData] = useReducer(formReducer, {
-    count: 69,
-    name: "Bruh",
-  });
-  const [submitting, setSubmitting] = useState(false);
+function TestForm() {
+  // Default formData is empty, this is used for the search bar
+  const [formData, setFormData] = useReducer(formReducer, {});
+  // Field is rendered once we search
+  const [searchResults, setSearchResults] = useState();
 
+  // Function handles a submit event
   const handleSubmit = (event) => {
+    // Don't perform a GET request
     event.preventDefault();
-    setSubmitting(true);
 
-    setTimeout(() => {
-      setSubmitting(false);
-      setFormData({
-        reset: true,
-      });
-    }, 3000);
-    /* fetch("/search")
+    // Make API request to get search results
+    fetch("/search?" + new URLSearchParams({
+      search_query: formData.search,
+      search_by: "title"
+    }))
       .then((response) => response.json())
       .then((data) => {
-
-      });*/
+        // Log data recieved for debug purposes
+        console.log(data);
+        // Render the search results there were returned
+        setSearchResults(
+          (
+            <div className="search">
+              <ul>
+                {data.map((hit) => (
+                  <li><b>Movie:</b> <i>{hit['_source'].title}</i>, <b>Description:</b> {hit['_source'].overview}</li>
+                ))}
+              </ul>
+            </div>
+          )
+        );
+      });
   };
 
   const handleChange = (event) => {
-    const isCheckbox = event.target.type === "checkbox";
     setFormData({
       name: event.target.name,
-      value: isCheckbox ? event.target.checked : event.target.value,
+      value: event.target.value,
     });
   };
 
   return (
     <div className="wrapper">
-      <h1>How About Them Apples</h1>
-      {submitting && (
-        <div>
-          You are submitting the following
-          <ul>
-            {Object.entries(formData).map(([name, value]) => (
-              <li key={name}>
-                <strong>{name}</strong>: {value.toString()}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+      <h1>Search a Movie</h1>
       <form onSubmit={handleSubmit}>
         <fieldset>
           <label>
-            Name:
+            Search:
             <input
-              name="name"
+              name="search"
               onChange={handleChange}
-              value={formData.name || ""}
-            />
-          </label>
-        </fieldset>
-        <fieldset>
-          <label>
-            <p>Apples</p>
-            <select
-              name="apple"
-              onChange={handleChange}
-              value={formData.apple || ""}
-            >
-              <option value="">--Please choose an option--</option>
-              <option value="fuji">Fuji</option>
-              <option value="jonathan">Jonathan</option>
-              <option value="honey-crisp">Honey Crisp</option>
-            </select>
-          </label>
-          <label>
-            <p>Count</p>
-            <input
-              type="number"
-              name="count"
-              onChange={handleChange}
-              step="1"
-              value={formData.count || ""}
-            />
-          </label>
-          <label>
-            <p>Gift Wrap</p>
-            <input
-              type="checkbox"
-              name="gift-wrap"
-              onChange={handleChange}
-              checked={formData["gift-wrap"] || false}
+              value={formData.search || ""}
             />
           </label>
         </fieldset>
         <button type="submit">Submit</button>
       </form>
+      { searchResults }
     </div>
   );
 }
@@ -123,7 +83,7 @@ function SamplePage() {
       <Link to="/" className="App-link">
         Back to Home Page
       </Link>
-      <MyForm />
+      <TestForm />
     </div>
   );
 }
