046ff9a7050e87266db3f472386bdbff966c7d57
Austin Hendricks <ahendricks@bbw.com>
2023-10-18 16:04:38 -0400

    Add autocomplete for searching by title

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,88 +1,127 @@
-import React, { useReducer, useState } from "react";
-import { Link } from "react-router-dom";
-import "../App.css";
-import { Button, Col, Card, Form, InputGroup } from "react-bootstrap";
+import React, { useReducer, useState } from 'react'
+import { Link } from 'react-router-dom'
+import '../App.css'
+import { Button, Col, Card, Form, InputGroup } from 'react-bootstrap'
 
 // Renders the results for a single search hit
-function SearchResult({ hit }) {
+function SearchResult ({ hit }) {
   // TODO: Make the rendering of the movies better include details like actors and genre
   return (
-    <Col className="search-entry">
+    <Col className='search-entry'>
       <Card>
         <Card.Body>
           <b>Movie: </b>
           <i>
-            <Link to={"/movies/" + hit["_id"]} className="App-link">
-              {hit["_source"].title}
+            <Link to={'/movies/' + hit['_id']} className='App-link'>
+              {hit['_source'].title}
             </Link>
           </i>
           <p>
-            <b>Description: </b> {hit["_source"].overview}
+            <b>Description: </b> {hit['_source'].overview}
           </p>
         </Card.Body>
       </Card>
     </Col>
-  );
+  )
+}
+
+function AutocompleteSuggestion ({ hit }) {
+  return (
+    <Col className='search-entry'>
+      <Card>
+        <Card.Body>
+          <i>{hit['_source'].title}</i>
+        </Card.Body>
+      </Card>
+    </Col>
+  )
 }
 
 // This function will update the JSON representation of the modifed value
-function formReducer(state, event) {
+function formReducer (state, event) {
   // event stores the name and value of the modified field, update it and leave
   // everything else unmodified
   return {
     ...state,
-    [event.name]: event.value,
-  };
+    [event.name]: event.value
+  }
 }
 
-function SearchForm() {
+function SearchForm () {
   // Default formData shouldn't be empty, this is used to hold all data from form
   const [formData, setFormData] = useReducer(formReducer, {
-    search_query: "",
-    search_by: "title",
-  });
+    search_query: '',
+    search_by: 'title'
+  })
   // Field is rendered once we search
-  const [searchResults, setSearchResults] = useState();
+  const [searchResults, setSearchResults] = useState()
+  const [autocompleteSuggestions, setAutocompleteSuggestions] = useState([])
 
   // Function for handling a submit request
-  const handleSubmit = (event) => {
+  const handleSubmit = event => {
     // Don't perform a GET request
-    event.preventDefault();
+    event.preventDefault()
 
     // Make API request to get search results
-    fetch("/search?" + new URLSearchParams(formData))
-      .then((response) => response.json())
-      .then((data) => {
+    fetch('/search?' + new URLSearchParams(formData))
+      .then(response => response.json())
+      .then(data => {
         // This block of code parses and renders the search results
         // Log data recieved for debug purposes
-        console.log(data);
+        console.log(data)
         // Render the search results that were returned
         setSearchResults(
-          <div className="search-results">
-            <ul className="search-entries">
+          <div className='search-results'>
+            <ul className='search-entries'>
               {
                 // For each hit we receive render an entry (title and description) for it
               }
-              {data.map((hit) => (
-                <SearchResult hit={hit} key={hit["_id"]} />
+              {data.map(hit => (
+                <SearchResult hit={hit} key={hit['_id']} />
               ))}
             </ul>
           </div>
-        );
-      });
-  };
+        )
+        // Clear autocomplete suggestions when search is triggered
+        setAutocompleteSuggestions([])
+      })
+  }
 
   // Given an event, this function sets up the name and value of the form component to be updated
-  const handleChange = (event) => {
+  const handleChange = event => {
+    const inputValue = event.target.value
+    const searchBy = formData.search_by
     setFormData({
       name: event.target.name,
-      value: event.target.value,
-    });
-  };
+      value: event.target.value
+    })
+
+    // Fetch autocomplete suggestions based on the input value and searchBy
+    fetch(`/autocomplete?search_query=${inputValue}&search_by=${searchBy}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data) // Log the data to check its structure
+        setAutocompleteSuggestions(data)
+      })
+      .catch(error => {
+        console.error('Error fetching autocomplete suggestions:', error)
+      })
+  }
+
+  const handleSuggestionClick = (event, suggestion) => {
+    // Prevent the default behavior of the event, e.g., preventing page refresh on click
+    event.preventDefault()
+    // Set the search query to the selected suggestion and trigger a search
+    setFormData({
+      name: 'search_query',
+      value: suggestion
+    })
+    handleSubmit(event)
+  }
 
   // Generate the HTML to return
   return (
-    <div className="search-form">
+    <div className='search-form'>
       <Form onSubmit={handleSubmit}>
         {
           // Basic search bar
@@ -94,17 +133,17 @@ function SearchForm() {
             // for the demo
           }
           <Form.Select
-            name="search_by"
+            name='search_by'
             onChange={handleChange}
-            style={{ width: "10%" }}
+            style={{ width: '10%' }}
           >
-            <option selected value="title">
+            <option selected value='title'>
               Title
             </option>
-            <option value="credits">Actor</option>
-            <option value="credits">Director</option>
-            <option value="genres">Genre</option>
-            <option value="production_companies">Production Company</option>
+            <option value='credits'>Actor</option>
+            <option value='credits'>Director</option>
+            <option value='genres'>Genre</option>
+            <option value='production_companies'>Production Company</option>
           </Form.Select>
           {
             // Adding this messes up the styling, figure out how to fix this
@@ -112,19 +151,19 @@ function SearchForm() {
             //<Form.Label for="search-bar">Search</Form.Label>
           }
           <Form.Control
-            name="search_query"
-            placeholder="Search"
+            name='search_query'
+            placeholder='Search'
             onChange={handleChange}
-            value={formData.search_query || ""}
-            id="search-bar"
-            style={{ width: "80%" }}
+            value={formData.search_query || ''}
+            id='search-bar'
+            style={{ width: '80%' }}
           />
           {
             // Adding this messes up the styling, figure out how to fix this
             // I think it's important for accessability?
             //<Form.Label>Search by</Form.Label>
           }
-          <Button type="submit" variant="primary">
+          <Button type='submit' variant='primary'>
             Submit
           </Button>
         </InputGroup>
@@ -138,12 +177,20 @@ function SearchForm() {
         }
       </Form>
       <br />
+      {/* Autocomplete suggestions */}
+      <ul className='autocomplete-suggestions'>
+        {autocompleteSuggestions.map(hit => (
+          <div className='clickable-card' onClick={handleSuggestionClick}>
+            <AutocompleteSuggestion hit={hit} key={hit['_id']} />
+          </div>
+        ))}
+      </ul>
       {
         // Renders the results of the search after submitted
         searchResults
       }
     </div>
-  );
+  )
 }
 
-export default SearchForm;
+export default SearchForm
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,43 +1,43 @@
-import React from "react";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import SearchForm from "../components/SearchForm";
+import React from 'react'
+import { Container, Row, Col, Card, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import SearchForm from '../components/SearchForm'
 
-function HomePage() {
+function HomePage () {
   return (
-    <Container fluid className="p-3">
-      <Row className="mb-3 mt-5">
+    <Container fluid className='p-3'>
+      <Row className='mb-3 mt-5'>
         {
           // Header of the website, has title and description of the website
         }
-        <Col className="mt-5">
-          <h1 className="large-bold-yellow">
+        <Col className='mt-5'>
+          <h1 className='large-bold-yellow'>
             <strong>Reel Good</strong>
           </h1>
-          <p className="lead text-white">
+          <p className='lead text-white'>
             The app that helps you find movies that you will enjoy based on your
             preferences and ratings.
           </p>
         </Col>
         <Col>
-          <div className="float-end">
-            <div className="btn-group">
-              <Button variant="success" as={Link} to="/login">
+          <div className='float-end'>
+            <div className='btn-group'>
+              <Button variant='success' as={Link} to='/login'>
                 Log in
               </Button>
-              <Button variant="primary" as={Link} to="/signup">
+              <Button variant='primary' as={Link} to='/signup'>
                 Sign up
               </Button>
             </div>
           </div>
         </Col>
       </Row>
-      <Row className="mb-3">
+      <Row className='mb-3'>
         {
           // Setup for the search form
         }
         <Col>
-          <Card bg="dark" text="white">
+          <Card bg='dark' text='white'>
             <Card.Body>
               <Card.Title>Search Movies</Card.Title>
               <SearchForm />
@@ -46,7 +46,7 @@ function HomePage() {
         </Col>
       </Row>
     </Container>
-  );
+  )
 }
 
-export default HomePage;
+export default HomePage
