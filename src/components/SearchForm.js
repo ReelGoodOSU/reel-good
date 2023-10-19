1b9b2db1eb37172fc84ec551f03a760e8c5cb8c0
michael <michael142857@163.com>
2023-10-19 14:26:45 -0400

    Add advance search for genre only

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,11 +1,8 @@
 import React, { useReducer, useState } from "react";
 import { Link } from "react-router-dom";
-import "../App.css";
 import { Button, Col, Card, Form, InputGroup } from "react-bootstrap";
 
-// Renders the results for a single search hit
 function SearchResult({ hit }) {
-  // TODO: Make the rendering of the movies better include details like actors and genre
   return (
     <Col className="search-entry">
       <Card>
@@ -25,10 +22,7 @@ function SearchResult({ hit }) {
   );
 }
 
-// This function will update the JSON representation of the modifed value
 function formReducer(state, event) {
-  // event stores the name and value of the modified field, update it and leave
-  // everything else unmodified
   return {
     ...state,
     [event.name]: event.value,
@@ -36,43 +30,26 @@ function formReducer(state, event) {
 }
 
 function SearchForm() {
-  // Default formData shouldn't be empty, this is used to hold all data from form
   const [formData, setFormData] = useReducer(formReducer, {
     search_query: "",
     search_by: "title",
   });
-  // Field is rendered once we search
-  const [searchResults, setSearchResults] = useState();
+  const [searchResults, setSearchResults] = useState([]);
+  const [availableGenres, setAvailableGenres] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
 
-  // Function for handling a submit request
   const handleSubmit = (event) => {
-    // Don't perform a GET request
     event.preventDefault();
-
-    // Make API request to get search results
     fetch("/search?" + new URLSearchParams(formData))
       .then((response) => response.json())
       .then((data) => {
-        // This block of code parses and renders the search results
-        // Log data recieved for debug purposes
-        console.log(data);
-        // Render the search results that were returned
-        setSearchResults(
-          <div className="search-results">
-            <ul className="search-entries">
-              {
-                // For each hit we receive render an entry (title and description) for it
-              }
-              {data.map((hit) => (
-                <SearchResult hit={hit} key={hit["_id"]} />
-              ))}
-            </ul>
-          </div>
-        );
+        const genres = [...new Set(data.map(hit => hit["_source"].genres))];
+        setAvailableGenres(genres);
+        setSearchResults(data);
+        setSelectedGenres([]); // Reset the selected genres after new search
       });
   };
 
-  // Given an event, this function sets up the name and value of the form component to be updated
   const handleChange = (event) => {
     setFormData({
       name: event.target.name,
@@ -80,37 +57,33 @@ function SearchForm() {
     });
   };
 
-  // Generate the HTML to return
+  const handleGenreChange = (event) => {
+    if (event.target.checked) {
+      setSelectedGenres(prevGenres => [...prevGenres, event.target.value]);
+    } else {
+      setSelectedGenres(prevGenres => prevGenres.filter(genre => genre !== event.target.value));
+    }
+  };
+
+  const filteredResults = selectedGenres.length > 0
+    ? searchResults.filter(hit => selectedGenres.includes(hit["_source"].genres))
+    : searchResults;
+
   return (
     <div className="search-form">
       <Form onSubmit={handleSubmit}>
-        {
-          // Basic search bar
-        }
         <InputGroup>
-          {
-            // TODO: I am hard coding the width of the search bar and select
-            // This is in no way a permanent solution, but I needed a quick fix
-            // for the demo
-          }
           <Form.Select
             name="search_by"
             onChange={handleChange}
             style={{ width: "10%" }}
           >
-            <option selected value="title">
-              Title
-            </option>
+            <option value="title">Title</option>
             <option value="credits">Actor</option>
             <option value="credits">Director</option>
             <option value="genres">Genre</option>
             <option value="production_companies">Production Company</option>
           </Form.Select>
-          {
-            // Adding this messes up the styling, figure out how to fix this
-            // I think it's important for accessability?
-            //<Form.Label for="search-bar">Search</Form.Label>
-          }
           <Form.Control
             name="search_query"
             placeholder="Search"
@@ -119,29 +92,31 @@ function SearchForm() {
             id="search-bar"
             style={{ width: "80%" }}
           />
-          {
-            // Adding this messes up the styling, figure out how to fix this
-            // I think it's important for accessability?
-            //<Form.Label>Search by</Form.Label>
-          }
           <Button type="submit" variant="primary">
             Submit
           </Button>
         </InputGroup>
-
-        {
-          // Note we are missing a label, I think it looks better without it. How can we include it to
-          // improve accessability without having it show up?
-          // TODO: It would be cool if we could use this for a drop-down menu
-          // https://react-bootstrap.github.io/docs/components/dropdowns/
-          // This is a dropdown menu for selecting what to search by
-        }
       </Form>
       <br />
-      {
-        // Renders the results of the search after submitted
-        searchResults
-      }
+      <div className="genre-filter">
+        {availableGenres.map(genre => (
+          <Form.Check
+            type="checkbox"
+            label={genre}
+            key={genre}
+            value={genre}
+            onChange={handleGenreChange}
+          />
+        ))}
+      </div>
+      <br />
+      <div className="search-results">
+        <ul className="search-entries">
+          {filteredResults.map((hit) => (
+            <SearchResult hit={hit} key={hit["_id"]} />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
