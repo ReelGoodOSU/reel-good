bc258d38ccc8d18497abee7f1d6653cb8c9f3d7a
Wesley <nickeley101@gmail.com>
2023-11-14 16:48:40 -0500

    Add new feature

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SamplePage from "./pages/SamplePage";
+import MovieDetails from "./pages/MovieDetails";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route exact path="/sample" element={<SamplePage />} />
+          <Route exact path="/movies/:id" element={<MovieDetails />} />
         </Routes>
       </div>
     </BrowserRouter>
