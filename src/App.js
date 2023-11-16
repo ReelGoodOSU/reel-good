3729b33d5394cbc31b77dfd721eb142813970a53
Joshua VonSeggern <joshvon44@gmail.com>
2023-11-16 14:47:42 -0500

    Remove unnecessary sample page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import SamplePage from "./pages/SamplePage";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <div className="App">
         <Routes>
           <Route exact path="/" element={<HomePage />} />
-          <Route exact path="/sample" element={<SamplePage />} />
         </Routes>
       </div>
     </BrowserRouter>
