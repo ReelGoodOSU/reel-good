e4a38ed7baed9122972ea7ae5f6c64b179772d60
Joshua VonSeggern <joshvon44@gmail.com>
2023-11-21 15:26:38 -0500

    Revamp how the page is loaded. Cards are now links and limited to only top 25, end is a list of movies

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,32 +3,21 @@ import { Link } from "react-router-dom";
 import { Button, Col, Card, Form, InputGroup } from "react-bootstrap";
 import "../App.css";
 
-function MovieCard({ id }) {
-  const [movie, setMovie] = useState(null);
-
-  // Load in the data for the movie, if we get an error, keep it as null
-  useEffect(() => {
-    fetch("/movies/" + id)
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.error) {
-          throw new Error("Could not find movie");
-        }
-        setMovie(data);
-        console.log(movie);
-      })
-      .catch((error) => {});
-  }, []);
-
+function MovieCard({ movie }) {
   // Format each movie entry as a card, just use image and title
   return (
     movie && (
       <Card>
-        <Card.Img
-          variant="top"
-          src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-        />
+        {movie.poster_path ? (
+          <Card.Img
+            variant="top"
+            src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+          />
+        ) : (
+          <p>No available poster</p>
+        )}
         <Card.Title>{movie.title}</Card.Title>
+        <a href={`/movies/${movie.id}`} className="stretched-link"></a>
       </Card>
     )
   );
--- src/pages/PersonDetails.js
+++ src/pages/PersonDetails.js
@@ -1,75 +1,125 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row, Col, Card, Button, Table } from "react-bootstrap";
+import { Container, Row, Col, Accordion } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import MovieCard from "../components/MovieCard";
 
 function PersonDetails() {
   // Take the id from the path of the URL and place in id variable
   const { id } = useParams();
-  const [personDetails, setPersonDetails] = useState();
+  const [person, setPerson] = useState();
+  const [movies, setMovies] = useState();
 
+  // Used to set up an array holding the movie entries for the person
   useEffect(() => {
+    if (person && person.credits) {
+      // Function used to fetch the data for all the movies the actor is in
+      const fetchMovies = async () => {
+        // Store array of movie data in movieDetails
+        const movieDetails = await Promise.all(
+          person.credits.map(async (movieId) => {
+            const response = await fetch(`/movies/${movieId}`);
+            if (!response.ok) {
+              return null;
+            }
+            const data = await response.json();
+            return data;
+          })
+        );
+        // Set movies variable to hold array of movie data, ignore null entries
+        setMovies(movieDetails.filter((entry) => entry !== null));
+      };
+
+      fetchMovies();
+    }
+  }, [person]);
+
+  // Used to load the data for a person
+  useEffect(() => {
+    // Fetch the data for the person with id
     fetch("/person/" + id)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data); // Log the data to check its structure
-
-        setPersonDetails(
-          <Container className="p-3 text-white">
-            <Row className="mb-3">
-              <Col>
-                <Image
-                  src={`https://image.tmdb.org/t/p/original/${data.profile_path}`}
-                  alt={data.name}
-                  width="250px"
-                  rounded
-                />
-              </Col>
-              <Col>
-                <h2>{data.name}</h2>
-                <p> {data.biography} </p>
-              </Col>
-              <Col>
-                <Container>
-                  <Row>
-                    <Col>Place of Birth: {data.place_of_birth}</Col>
-                  </Row>
-                  <Row>
-                    <Col>Birthday: {data.birthday}</Col>
-                  </Row>
-                  {data.deathday != null && (
-                    <Row>
-                      <Col>Death: {data.deathday}</Col>
-                    </Row>
-                  )}
-                </Container>
-              </Col>
-            </Row>
-            <Row className="flex-nowrap overflow-auto" sm={4}>
-              {data.credits.map((movieId) => {
-                return (
-                  <Col>
-                    <MovieCard id={movieId} />
-                  </Col>
-                );
-              })}
-            </Row>
-          </Container>
-        );
+        // Set the person variable
+        setPerson(data);
       })
       .catch((error) => {
         console.error("Error fetching person:", error);
       });
-  }, []);
+  }, [id]);
 
-  return (
-    <div>
-      {
-        // Page is rendered by feting data from backend
-      }
-      {personDetails}
-    </div>
+  return person ? (
+    <Container className="p-3 text-white">
+      <Row className="mb-3">
+        <Col>
+          <Image
+            src={`https://image.tmdb.org/t/p/original/${person.profile_path}`}
+            alt={person.name}
+            width="250px"
+            rounded
+          />
+        </Col>
+        <Col>
+          <h2>{person.name}</h2>
+          <p> {person.biography} </p>
+        </Col>
+        <Col>
+          <Container>
+            <Row>
+              <Col>Place of Birth: {person.place_of_birth}</Col>
+            </Row>
+            <Row>
+              <Col>Birthday: {person.birthday}</Col>
+            </Row>
+            {person.deathday != null && (
+              <Row>
+                <Col>Death: {person.deathday}</Col>
+              </Row>
+            )}
+          </Container>
+        </Col>
+      </Row>
+      <Row className="flex-nowrap overflow-auto" sm={4}>
+        {movies ? (
+          movies.slice(0, 25).map((movie) => {
+            return (
+              <Col>
+                <MovieCard movie={movie} />
+              </Col>
+            );
+          })
+        ) : (
+          <p>Loading ...</p>
+        )}
+      </Row>
+      <Row>
+        <Accordion>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Full Credits List</Accordion.Header>
+            <Accordion.Body>
+              {movies ? (
+                <ul>
+                  {
+                    // Render a list of movies that the person was involved in
+                    movies.map((movie) => {
+                      return (
+                        <li key={movie.id}>
+                          <a href={`/movies/${movie.id}`}>{movie.title}</a>
+                        </li>
+                      );
+                    })
+                  }
+                </ul>
+              ) : (
+                <p>Loading ...</p>
+              )}
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </Row>
+    </Container>
+  ) : (
+    <p className="lead text-white">Loading...</p>
   );
 }
 
